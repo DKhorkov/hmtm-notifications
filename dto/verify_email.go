581a70ea662f7be6f9b9c52d8dec725a7ea111ef
DKhorkov <dkhorkov.dev@gmail.com>
2025-02-21 00:05:46 +0300

    [HMTM-98] - change password

--- dto/verify_email.go
+++ dto/verify_email.go
@@ -3,8 +3,3 @@ package dto
 type VerifyEmailDTO struct {
 	UserID uint64 `json:"user_id"`
 }
-
-type ForgetPasswordDTO struct {
-	UserID      uint64 `json:"user_id"`
-	NewPassword string `json:"new_password"`
-}
